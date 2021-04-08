c8ec80ecd34bbad60979b31744ecfc149efdfee2
fuka100 <70669446+fuka100@users.noreply.github.com>
2021-04-08 11:29:55 +0200

    Setup of a sql table.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Billett
+(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    film VARCHAR(50) NOT NULL,
+    antall VARCHAR(10) NOT NULL,
+    fornavn VARCHAR(100) NOT NULL,
+    etternavn VARCHAR(100) NOT NULL,
+    telefonnr VARCHAR(50) NOT NULL,
+    epost VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
