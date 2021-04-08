55993de47e6bbc08256a4b9be82ab7b906e64574
fuka100 <70669446+fuka100@users.noreply.github.com>
2021-04-08 11:17:41 +0200

    Added comments in the repository class.

--- src/main/java/com/example/demo/BillettRepository.java
+++ src/main/java/com/example/demo/BillettRepository.java
@@ -13,18 +13,21 @@ public class BillettRepository {
     @Autowired
     private JdbcTemplate db;
 
+    // Store ticket info in the database
     public void lagreBillett(Billett innBillett) {
         String sql = "INSERT INTO Billett (film, antall, fornavn, etternavn, telefonnr, epost) VALUES(?,?,?,?,?,?)";
         db.update(sql, innBillett.getFilm(), innBillett.getAntall(), innBillett.getFornavn(),
                 innBillett.getEtternavn(), innBillett.getTelefonnr(), innBillett.getEpost());
     }
 
+    // Retrieve data from the database
     public List<Billett> hentBillett() {
         String sql = "SELECT * FROM Billett";
         List<Billett> billettListe = db.query(sql, new BeanPropertyRowMapper(Billett.class));
         return billettListe;
     }
 
+    // Delete data in the database
     public void slettBillett() {
         String sql = "DELETE FROM Billett";
         db.update(sql);
